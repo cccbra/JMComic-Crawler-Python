412c7caa519f147ea336f3793185d73ead826765
cccbra <690176177@qq.com>
2025-03-24 23:47:28 +0800

    Update workflow_download.py

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -3,10 +3,22 @@ from jmcomic.cl import JmcomicUI
 
 # 下方填入你要下载的本子的id，一行一个，每行的首尾可以有空白字符
 jm_albums = '''
-54575
+
+
+505370
+
+
 
 
 '''
+
+# 单独下载章节
+jm_photos = '''
+
+
+
+'''
+
 
 def env(name, default, trim=('[]', '""', "''")):
     import os
